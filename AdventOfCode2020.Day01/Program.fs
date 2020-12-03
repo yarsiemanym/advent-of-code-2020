b14b18f8fa6d21eec5005a210e2576e4d5faf669
Ray Saltrelli <ray@saltrelli.com>
2020-12-03 17:56:28 -0500

    Do it in a more functional way.

--- AdventOfCode2020.Day01/Program.fs
+++ AdventOfCode2020.Day01/Program.fs
@@ -1,45 +1,43 @@
 ﻿module AdventOfCode2020.Day01
 
-open System
 open System.IO
 
-let readFile (filePath:string) = 
-    [
-        use sr = new StreamReader(filePath)
-        while not sr.EndOfStream do
-            yield Int32.Parse(sr.ReadLine ())
-    ]
+let readFile =
+    File.ReadAllLines 
+    >> Array.map int 
+    >> Array.toSeq
 
-let sum (a:int, b:int, c:int) = a + b + c
+let sum (a, b, c) = a + b + c
 
-let product (a:int, b:int, c:int) = a * b * c
+let product (a, b, c) = a * b * c
 
-let sumEquals2020 (a:int, b:int, c:int) = sum(a, b, c) = 2020
+let sumEquals2020 (a, b, c) = sum (a, b, c) = 2020
 
-let buildTuples (entries:list<int>) = 
-    [
-        for i in 0 .. entries.Length - 1 do
-            for j in 0 .. entries.Length - 1 do
-                for k in 0 .. entries.Length - 1 do
-                    if i <> j && j <> k  && i <> k then
-                        yield (entries.Item(i), entries.Item(j), entries.Item(k))
-    ]
+let buildTuples entries =
+    seq {
+        for i in 0 .. Seq.length (entries) - 1 do
+            for j in 0 .. Seq.length (entries) - 1 do
+                for k in 0 .. Seq.length (entries) - 1 do
+                    if i <> j && j <> k && i <> k
+                    then yield (Seq.item i entries, Seq.item j entries, Seq.item k entries)
+    }
 
-let inspectTuples (tuples:list<int * int * int>) = 
-    [
+let inspectTuples tuples =
+    seq {
         for tuple in tuples do
-            if sumEquals2020(tuple) then
-                yield product(tuple)
-    ]
-    
-let findAnswers (entries:list<int>) = buildTuples(entries) |> inspectTuples
+            if sumEquals2020 (tuple) then yield product (tuple)
+    }
 
-let findAnswer (entries:list<int>) =  findAnswers(entries).Head
+let printAnswer answer = printfn "The answer is '%d'." answer
 
-let printAnswer (answer) =
-    printfn "The answer is %d." answer
-    0
+let findAnswer =
+    readFile
+    >> buildTuples
+    >> inspectTuples
+    >> Seq.head
+    >> printAnswer
 
 [<EntryPoint>]
 let main argv =
-    readFile(argv.[0]) |> findAnswer |> printAnswer
+    findAnswer argv.[0]
+    0
