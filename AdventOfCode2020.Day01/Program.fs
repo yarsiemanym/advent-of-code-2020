9fc6a86cbefa9cadebdb79f372207c4a4b8e3e73
Ray Saltrelli <ray@saltrelli.com>
2020-12-03 16:54:31 -0500

    Day 1 part 1 complete.

---
+++ AdventOfCode2020.Day01/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+open System.IO
+
+let readFile (filePath:string) = 
+    [
+        use sr = new StreamReader(filePath)
+        while not sr.EndOfStream do
+            yield Int32.Parse(sr.ReadLine ())
+    ]
+
+let sum (a:int, b:int) = a + b
+
+let product (a:int, b:int) = a * b
+
+let sumEquals2020 (a:int, b:int) = sum(a, b) = 2020
+
+let buildTuples (entries:list<int>) = 
+    [
+        for i in 0 .. entries.Length - 1 do
+            for j in 0 .. entries.Length - 1 do
+                if i <> j then
+                    yield (entries.Item(i), entries.Item(j))
+    ]
+
+let inspectTuples (tuples:list<int * int>) = 
+    [
+        for tuple in tuples do
+            if sumEquals2020 tuple then
+                yield product tuple
+    ]
+    
+let findAnswers (entries:list<int>) = buildTuples(entries) |> inspectTuples
+
+let findAnswer (entries:list<int>) =  findAnswers(entries).Head
+
+let printAnswer (answer) =
+    printfn "The answer is %d." answer
+    0
+
+[<EntryPoint>]
+let main argv =
+    readFile(argv.[0]) |> findAnswer |> printAnswer
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# advent-of-code-2020
+# Advent of Code 2020
+
+Using the [Advent of Code 2020](https://adventofcode.com/2020) puzzles to learn [F#](https://fsharp.org/). 
