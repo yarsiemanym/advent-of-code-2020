ef095d47ed33d2612eeeb6a133ce2083399b5b71
Ray Saltrelli <ray@saltrelli.com>
2020-12-04 13:46:28 -0500

    Clean things up.

--- AdventOfCode2020.Day03/Program.fs
+++ AdventOfCode2020.Day03/Program.fs
@@ -2,74 +2,65 @@
 
 open System.IO
 
-type Point = {
-    X: int
-    Y: int
-}
+type Point = { X: int; Y: int }
 
-let parseLine line = Seq.toArray line
+let parseLine line = Seq.toList line
 
 let parseLines lines =
-    [|
+    [ 
         for line in lines do
             yield parseLine line
-    |]
+    ]
 
-let readFile =
+let readFile = 
     File.ReadAllLines 
     >> parseLines
 
-let heightOf (map:array<array<char>>) = map.Length
+let heightOf (map: list<list<char>>) = map.Length
 
-let widthOf (map:array<array<char>>) = map.[0].Length
+let widthOf (map: list<list<char>>) = map.[0].Length
 
-let check point (map:array<array<char>>) = map.[point.Y].[point.X % widthOf map]
+let check (point, map: list<list<char>>) = map.[point.Y].[point.X % widthOf map]
 
-let constructPath (slope, map:array<array<char>>) =
-    ( 
+let constructPath (slope, map: list<list<char>>) =
+    (
         seq {
             for i in 0 .. (heightOf map / slope.Y) - 1 do
-                yield { X = i * slope.X; Y = i * slope.Y}
-        },
-        map
+                yield { X = i * slope.X; Y = i * slope.Y }
+         },
+         map
     )
 
-let isTree point (map:array<array<char>>) = check point map = '#'
+let isTree (point, map: list<list<char>>) = check (point, map) = '#'
 
-let findTreesOnPath (path, map:array<array<char>>) =
+let findTreesOnPath (path, map: list<list<char>>) =
     seq {
         for point in path do
-            if isTree point map then yield 'X'
-            else yield 'O'
+            if isTree (point, map) then yield 'X' else yield 'O'
     }
 
-let countTrees =
-    Seq.filter ((=) 'X') 
-    >> Seq.length
-
+let countTrees = Seq.filter ((=) 'X') >> Seq.length
 
 let printAnswer answer = printfn "The answer is '%u'." answer
 
-let findAnswer = 
-    constructPath
+let findAnswer =
+    constructPath 
     >> findTreesOnPath
     >> countTrees
 
-
 [<EntryPoint>]
 let main argv =
-    let slopes = [
-        {X=1; Y=1}
-        {X=3; Y=1}
-        {X=5; Y=1}
-        {X=7; Y=1}
-        {X=1; Y=2}
-    ]
+    let slopes =
+        [ { X = 1; Y = 1 }
+          { X = 3; Y = 1 }
+          { X = 5; Y = 1 }
+          { X = 7; Y = 1 }
+          { X = 1; Y = 2 } ]
 
-    let map = readFile argv.[0] 
+    let map = readFile argv.[0]
 
     let treeCounts =
-        [
+        [ 
             for slope in slopes do
                 yield findAnswer (slope, map)
         ]
