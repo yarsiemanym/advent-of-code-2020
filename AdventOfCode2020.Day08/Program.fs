b44c3cf647ec4edf256880825ac309ca122e710d
Ray Saltrelli <ray@saltrelli.com>
2020-12-08 13:35:12 -0500

    Day 8 part 2 complete.

--- AdventOfCode2020.Day08/Program.fs
+++ AdventOfCode2020.Day08/Program.fs
@@ -5,7 +5,7 @@ open System.IO
 open System.Text.RegularExpressions
 
 type Instruction = {
-    Operation:string
+    mutable Operation:string
     Value:int
     mutable HasExecuted:bool
 }
@@ -23,7 +23,7 @@ let parseInstruction text =
     else
         raise (Exception (sprintf "Invalid instruction '%s'" text))
 
-let parseInstructions lines =
+let parseInstructionSet lines =
     [
         for line in lines do
             yield parseInstruction line
@@ -31,15 +31,15 @@ let parseInstructions lines =
 
 let readFile =
     File.ReadAllLines
-    >> parseInstructions
+    >> parseInstructionSet
 
-let executeInstructions (instructions:list<Instruction>) =
+let executeInstructionSet (instructionSet:list<Instruction>) =
     let mutable pointer = 0
     let mutable continueLoop = true
     let mutable accumulator = 0
 
     while continueLoop do
-        let instruction = instructions.Item(pointer)
+        let instruction = instructionSet.Item(pointer)
 
         if not instruction.HasExecuted then
         
@@ -50,18 +50,58 @@ let executeInstructions (instructions:list<Instruction>) =
             | _ -> raise (Exception (sprintf "Unsupported operation '%s'" instruction.Operation))
             
             instruction.HasExecuted <- true
-            continueLoop <- pointer < instructions.Length
+            continueLoop <- pointer < instructionSet.Length
         else
-            printfn "Infinite loop detected at position %d" pointer
-            continueLoop <- false
+            raise (Exception (sprintf "Infinite loop detected at position %d" pointer))
     
     accumulator
+
+let copyInstructionSet instructionSet = 
+    [
+        for instruction in instructionSet do
+            yield {
+                Operation = instruction.Operation
+                Value = instruction.Value
+                HasExecuted = instruction.HasExecuted
+            }
+    ]
+
+let modifyOperation instruction = 
+    match instruction.Operation with
+    | "nop" -> instruction.Operation <- "jmp"
+    | "jmp" -> instruction.Operation <- "nop"
+    | _ -> ()
+
+let buildModifiedInstructionSet (instructionSet:list<Instruction>, indexToModify) =
+    let copyOfInstructions = copyInstructionSet instructionSet
+    let instruction = copyOfInstructions.Item(indexToModify)
+    modifyOperation instruction
+    copyOfInstructions
+
+let buildModifiedInstructionSets (instructionSet:list<Instruction>) =
+    seq {
+        for i in 0 .. instructionSet.Length - 1 do
+            buildModifiedInstructionSet (instructionSet, i)
+    }
+
+let testModifiedInstructionSet (instructionSet:list<Instruction>) =
+    try
+        (true, executeInstructionSet instructionSet)
+    with
+        | ex -> (false, 0)
+
+let testModifiedInstructionSets (instructionSets:seq<list<Instruction>>) = 
+    Seq.map (testModifiedInstructionSet) instructionSets
+    |> Seq.filter (fun result -> fst(result))
+    |> Seq.head
+    |> snd
 
 let printAnswer answer = printfn "The answer is '%d'." answer
 
 let findAnswer = 
     readFile
-    >> executeInstructions
+    >> buildModifiedInstructionSets
+    >> testModifiedInstructionSets
     >> printAnswer
 
 [<EntryPoint>]
