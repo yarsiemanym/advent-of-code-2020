81e7a254505f0519d2fa8860fa3c500f390eb978
Ray Saltrelli <ray@saltrelli.com>
2020-12-09 10:24:40 -0500

    Day 9 part 1 complete.

---
+++ AdventOfCode2020.Day09/Program.fs
@@ -0,0 +1,40 @@
+﻿module AdventOfCode2020.Day09
+
+open System
+open System.IO
+
+let readFile = 
+    File.ReadAllLines
+    >> Array.map (int)
+    >> Array.toList
+
+let printAnswer answer = printfn "The answer is '%d'." answer
+
+let isValid number (preamble:list<int>) = 
+    let mutable isValid = false
+    for i in 0 .. preamble.Length - 1 do
+        for j in 0 .. preamble.Length - 1 do
+            if i <> j && preamble.[i] + preamble.[j] = number then
+                isValid <- true
+    isValid
+
+let validateNumbers preambleLength (numbers:list<int>) = 
+    seq {
+        for i in preambleLength .. numbers.Length - 1 do
+            let number = numbers.Item i
+            (number, isValid number numbers.[i-preambleLength-1 .. i-1])
+    }
+
+
+let findAnswer preambleLength = 
+    readFile
+    >> validateNumbers preambleLength
+    >> Seq.filter (not << snd)
+    >> Seq.map (fst)
+    >> Seq.head
+    >> printAnswer
+
+[<EntryPoint>]
+let main argv =
+    findAnswer (int argv.[0]) argv.[1]
+    0
