4b643c317b87e77d3266d55faa8dbc3f5e33c3d0
Ray Saltrelli <ray@saltrelli.com>
2020-12-09 12:26:25 -0500

    Day 9 part 2 complete.

--- AdventOfCode2020.Day09/Program.fs
+++ AdventOfCode2020.Day09/Program.fs
@@ -1,14 +1,11 @@
 ﻿module AdventOfCode2020.Day09
 
-open System
 open System.IO
 
 let readFile = 
     File.ReadAllLines
     >> Array.map (int64)
     >> Array.toList
-
-let printAnswer answer = printfn "The answer is '%d'." answer
 
 let isValid number (preamble:list<int64>) = 
     let mutable isValid = false
@@ -22,17 +19,41 @@ let validateNumbers preambleLength (numbers:list<int64>) =
     seq {
         for i in preambleLength .. numbers.Length - 1 do
             let number = numbers.Item i
-            (number, isValid number numbers.[i-preambleLength-1 .. i-1])
+            (number, isValid number numbers.[i - preambleLength - 1 .. i - 1])
     }
 
-
-let findAnswer preambleLength = 
-    readFile
-    >> validateNumbers preambleLength
+let findInvalidNumber preambleLength =
+    validateNumbers (preambleLength)
     >> Seq.filter (not << snd)
     >> Seq.map (fst)
     >> Seq.head
-    >> printAnswer
+
+let calcContiguousSum agg number = ((List.append [number] (fst agg )), (snd agg) + number)
+    
+
+let calcContiguousSums (numbers:list<int64>) target = 
+    seq {
+        for i in 0 .. numbers.Length - 1 do
+            let contiguousNumbers = numbers.[i .. numbers.Length - 1]
+            let candidates = List.scan calcContiguousSum (List.Empty, 0L) contiguousNumbers
+
+            for candidate in candidates do
+                if snd candidate = target then
+                    yield fst candidate
+    }
+
+let calcXmasWeakness (numbers:list<int64>) = (List.min numbers) + (List.max numbers)
+
+let printAnswer answer = printfn "The answer is '%d'." answer
+
+let findAnswer preambleLength filePath = 
+    let numbers = readFile filePath
+    
+    findInvalidNumber preambleLength numbers
+    |> calcContiguousSums numbers
+    |> Seq.head
+    |> calcXmasWeakness
+    |> printAnswer
 
 [<EntryPoint>]
 let main argv =
