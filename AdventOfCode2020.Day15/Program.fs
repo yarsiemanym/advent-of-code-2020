9f375fabd1d50b1bc62eef21df0638cf6b6e5345
Ray Saltrelli <ray@saltrelli.com>
2020-12-17 17:42:42 -0500

    Day 15 part 2 complete.

--- AdventOfCode2020.Day15/Program.fs
+++ AdventOfCode2020.Day15/Program.fs
@@ -1,39 +1,55 @@
 ﻿namespace AdventOfCode2020.Day15
 
 open System
+open System.Collections.Generic
 open System.IO
 
 module Program = 
 
+    let mutable rememberedNumbers = new Dictionary<int, int>()
+
     let readFile filePath = 
         (File.ReadAllText filePath).Split ','
         |> Array.toList
-        |> List.map (uint64)
-        |> List.rev
+        |> List.map (int)
 
-    let ageOfNumber spokenNumbers number = 
-        if List.tail spokenNumbers |> List.contains number then
-            List.findIndex (fun n -> n = number) (List.tail spokenNumbers)
-            |> (+) 1
-            |> uint64
+    let ageOfNumber number currentRound = 
+        if rememberedNumbers.ContainsKey(number) then
+            currentRound - 1 - rememberedNumbers.[number]
         else
-            0UL
+            0
 
-    let speakNextNumber spokenNumbers = 
-        List.head spokenNumbers
-        |> ageOfNumber spokenNumbers
-        |> (fun nextNumber -> nextNumber :: spokenNumbers)
+    let rememberNumber number round = 
+        rememberedNumbers.Remove(number) |> ignore
+        rememberedNumbers.Add(number, round)
 
-        
+    let initializeGame startingNumbers =
+        rememberedNumbers <- new Dictionary<int, int>()
+        for i in 0 .. (List.length startingNumbers) - 1 do
+            let number = List.item i startingNumbers
+            rememberNumber number (i + 1)
+
+    let playGame initialState numberOfRounds =
+        List.take (List.length initialState - 1) initialState
+        |> initializeGame
+
+        let mutable lastNumber = List.last initialState
+
+        for roundNumber in initialState.Length + 1 .. numberOfRounds do
+            let nextNumber = ageOfNumber lastNumber roundNumber
+            rememberNumber lastNumber (roundNumber - 1)
+            lastNumber <- nextNumber
+
+        lastNumber
 
     [<EntryPoint>]
     let main argv =
-        let mutable spokenNumbers = readFile argv.[0]
-        let target = int argv.[1]
+        let mutable initialState = readFile argv.[0]
 
-        while (List.length spokenNumbers) < target do
-            spokenNumbers <- speakNextNumber spokenNumbers
+        playGame initialState 2020
+        |> printfn "The answer to part 1 is '%d'."
 
-        printfn "The answer to part 1 is '%d'." (List.head spokenNumbers)
+        playGame initialState 30000000
+        |> printfn "The answer to part 2 is '%d'."
 
         0 
