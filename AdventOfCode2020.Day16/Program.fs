2d27fd0431a27dc085c58f1ef170d5ab1e88583c
Ray Saltrelli <ray@saltrelli.com>
2020-12-19 11:50:44 -0500

    Day 16 part 2 complete.

--- AdventOfCode2020.Day16/Program.fs
+++ AdventOfCode2020.Day16/Program.fs
@@ -7,12 +7,37 @@ open AdventOfCode2020.Day16.Train
 
 module Program = 
 
+    type FieldAnalysis =
+        {
+            FieldName:string
+            PossibleIndices:list<int>
+        }
+
+        member this.LockedIn = this.PossibleIndices.Length = 1
+
     type Input = 
         {
             Fields: list<Field>
             MyTicket: Ticket
             NearbyTickets: list<Ticket>
         }
+
+        member this.ValidTickets = List.filter (fun (ticket:Ticket) -> ticket.IsValid this.Fields) this.NearbyTickets
+
+        member this.AnalyzeFields = 
+            [
+                for field in this.Fields do
+                    yield  
+                        {
+                            FieldName = field.Name
+                            PossibleIndices =
+                                [
+                                    for i in 0 .. this.ValidTickets.[0].Values.Length - 1 do
+                                        if List.forall (fun (ticket:Ticket) -> field.IsValid ticket.Values.[i]) this.ValidTickets then
+                                            yield i
+                                ]
+                        }
+            ]
 
     let parseTicketSection (text:string) =
         let lines = text.Split('\n', StringSplitOptions.RemoveEmptyEntries)
@@ -33,12 +58,35 @@ module Program =
         else
             failwithf "Invalid input."
 
-
     let readFile =
         File.ReadAllText
         >> parseInput
 
-    let calcPart1Answer input = List.sumBy (fun (ticket:Ticket) -> ticket.ErrorRate input.Fields) input.NearbyTickets
+    let calcPart1Answer input = List.sumBy (fun (ticket:Ticket) -> ticket.ErrorRate input.Fields) input.NearbyTickets   
+
+    let lockInNextField analysis = 
+        let lockedIndices = List.choose (fun (field:FieldAnalysis) -> if field.LockedIn then Some field.PossibleIndices.Head else None) analysis
+        [
+            for field in analysis do
+                if field.LockedIn then
+                    yield field
+                else
+                    yield { field with PossibleIndices = List.except lockedIndices field.PossibleIndices }
+        ]
+
+    let identifyFields (initialAnalysis:list<FieldAnalysis>) = 
+        let mutable progressiveAnalysis = initialAnalysis
+        let mutable continueLoop = true
+
+        while continueLoop do
+            progressiveAnalysis <- lockInNextField progressiveAnalysis
+            continueLoop <- List.exists (fun (field:FieldAnalysis) -> not field.LockedIn) progressiveAnalysis
+
+        List.map (fun (field:FieldAnalysis) -> (field.FieldName, field.PossibleIndices.Head)) progressiveAnalysis        
+
+    let calcPart2Answer myTicket = 
+        List.choose (fun (field:string*int) -> if (fst field).StartsWith("departure") then Some (uint64 myTicket.Values.[snd field]) else None)
+        >> List.fold (*) 1UL
 
     [<EntryPoint>]
     let main argv =
@@ -46,5 +94,10 @@ module Program =
 
         calcPart1Answer input
         |> printfn "The answer to part 1 is '%d'."
+
+        input.AnalyzeFields
+        |> identifyFields
+        |> calcPart2Answer input.MyTicket
+        |> printfn "The answer to part 2 is '%d'."
 
         0
