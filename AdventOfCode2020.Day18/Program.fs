e212b26d0045480f24c843814f874850a5652a6e
Ray Saltrelli <ray@saltrelli.com>
2020-12-21 14:22:01 -0500

    Setup for starting part 2 work.

--- AdventOfCode2020.Day18/Program.fs
+++ AdventOfCode2020.Day18/Program.fs
@@ -2,18 +2,20 @@
 
 open System
 open System.IO
-open AdventOfCode2020.Day18.Math
 
 module Program =
 
-    let readFile =
-        File.ReadAllLines
-        >> parseExpressions
-
     [<EntryPoint>]
     let main argv =
-        readFile argv.[0]
+        
+        File.ReadAllLines argv.[0]
+        |> MathV1.parseExpressions
         |> List.sumBy (fun expression -> expression.Evaluate)
         |> printfn "The answer to part 1 is '%d'."
+
+        File.ReadAllLines argv.[0]
+        |> MathV2.parseExpressions
+        |> List.sumBy (fun expression -> expression.Evaluate)
+        |> printfn "The answer to part 2 is '%d'."
         
         0 
